766dfb837257d43e757f8f752b824b16019a1743
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-11 15:20:15 -0700

    2nd C session

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description: 'assign random number to n and  compare random number'
+ * Return: always  0 success
+*/
+int main(void)
+{
+	int l = 0;
+
+	while (l <= 9)
+	{
+		printf("%d", l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
