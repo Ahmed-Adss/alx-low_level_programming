9e226161a968413a5ed428994cc84a4d88d7f43c
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-11 14:55:52 -0700

    2nd C session

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Description: 'assign random number to n and  compare random number'
+ * Return: always  0 success
+*/
+int main(void)
+{
+	char l = 97;
+
+	while (l <= 122)
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
