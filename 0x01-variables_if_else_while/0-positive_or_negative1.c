b876a8b62b9785ec61d147fb2185628d5ed2ac42
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-11 13:41:31 -0700

    2nd C session

--- 0x01-variables_if_else_while/0-positive_or_negative1.c
+++ 0x01-variables_if_else_while/0-positive_or_negative1.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
-#include <stdio.h>
-/* betty style doc for function main goes there */
+#include<stdio.h>
 /**
  * main- entry point
  * Description: compare random number
@@ -11,10 +7,6 @@
 int main(void)
 {
 	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there*/
 
 	if (n == 0)
 	{
