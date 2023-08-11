158bd0b1075827168a69c573f335a48e0c4f2a77
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-10 18:52:06 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <unistd.h>
+#include<unistd.h>
 /**
  * main- entry
+ *
  * Description: write function
  * Return: 1
 */
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 
 	write(1, quo, 59);
 	return (1);
