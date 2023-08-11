7da931843cd097fbee97409336606fb60e6d52fb
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-11 13:11:36 -0700

    2nd c session

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+/**
+ * main- entry point
+ * Description: compare random number
+ * Return : 0 success
+*/
+int main(void)
+{
+	int n;
+
+	if (n == 0)
+	{
+		printf("%d is zero", n);
+	} else if (n < 0)
+	{
+		printf("%d is negative", n);
+	} else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+this is a variable, if, esle, and while session
