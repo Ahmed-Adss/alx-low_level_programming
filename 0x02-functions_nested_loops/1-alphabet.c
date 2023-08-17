cd02d3df4a8700b592a04a2d0902bc2e9d455b52
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-16 23:11:01 -0700

    functions session

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet- entry point
+*Description: 'print lower case alphabet'
+*Return: always 0 success
+*/
+
+void print_alphabet(void)
+{
+	int char;
+
+	for (char = 'a' ; char <= 'z' ; char++)
+		{
+			_putchar('char');
+		}
+}
