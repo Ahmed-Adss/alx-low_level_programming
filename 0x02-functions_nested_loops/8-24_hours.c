00b770542215dc27f3975f668a3babad673e9c91
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-17 03:32:43 -0700

    functions session

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,14 +11,15 @@ void jack_bauer(void)
 int min1, min2, hr1, hr2;
 for (hr1 = 0;  hr <= 2; hr++)
 {
-_putchar('0' + hr1);
 for (hr2 = 0; hr2 <= 9; hr2++)
 {
-_putchar('0' + hr2);
 for (min1 = 0; min1 <= 6; min1++)
 {
-_putchar('0' + min1);
 for (min2 = 0; min2 <= 9; min2++)
+_putchar('0' + hr1);
+_putchar('0' + hr2);
+_putchar(58);
+_putchar('0' + min1);
 _putchar('0' + min2);
 _putchar('\n');
 }
