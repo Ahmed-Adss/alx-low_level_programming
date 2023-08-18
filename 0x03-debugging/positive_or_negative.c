7b79c0ccfa4de239699becd251eb41918a4f324c
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-18 04:11:13 -0700

    Debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+/* betty style doc for function main goes there */
+/**
+ * main- entry point
+ * Description: 'assign random number to n and  compare random number'
+ * Return: always  0 success
+*/
+#include "main.h"
+void positive_or_negative(int n)
+{
+        if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        } else if (n < 0)
+        {
+                printf("%d is negative\n", n);
+        } else if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        return;
+}
