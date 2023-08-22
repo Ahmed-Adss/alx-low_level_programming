bd0c73c4b89e3cc10b854461fba2723be825e719
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-22 14:42:03 -0700

    pointers_arrays_strings session

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+*reset_to_98 - updates the value it points to to 98
+*@n: variable changed to 98
+*Return: void
+*/
+
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+	[
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
