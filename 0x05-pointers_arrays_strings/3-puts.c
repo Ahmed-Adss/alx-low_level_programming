b292f19385deb1196b0695a2cd1c46b405129f82
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-22 00:12:41 -0700

    pointers_arrays_strings session

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+*_puts - hat prints a string, followed by a new line
+*@str: variable input
+*Return: void
+*/
+
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; str[i] =! '\0'; i++)
+	{
+		_putchar('str[i]');
+	}
+
+}
