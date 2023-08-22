f62e18224c3b40dc75d657c97d02f860c51e6253
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-22 12:09:52 -0700

    pointers_arrays_strings session

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,17 @@ void rev_string(char *s)
 {
 	int i;
 	int count = 0;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		count++;
 	}
-	for (i = count-1; i >= 0; i--)
+	for (i = 0; i < count/2; i++)
 	{
-		_putchar(s[i]);
+		char j;
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = j;
 	}
-	_putchar('\n');
-	return;
-
-
 
 }
