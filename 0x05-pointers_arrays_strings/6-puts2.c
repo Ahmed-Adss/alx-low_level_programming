72c1511c739e9ec29d76bf9fd46b2c730ba84396
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-08-22 12:31:57 -0700

    pointers_arrays_strings session

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include"main.h"
+
+/*
+*puts2 - prints every other character of a string
+*@str: variable printed
+*Return: void
+*/
+
+void puts2(char *str)
+{
+	int i;
+	int count = 0;
+	for (i = 0; i != '\0'; i++)
+	{
+		count++;
+	}
+
+	for (i = 0; i <= count ; i+=2)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+
+
+
+
+}
