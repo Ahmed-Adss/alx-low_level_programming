04023f12067fbd3299393010585bd0a14bd1ee2a
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-01 07:49:42 -0700

    argc_argv session

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int result1, result2;
+
+	if (argc == 3)
+	{
+		result1 = atoi(argv[1]);
+		result2 = atoi(argv[2]);
+		printf("%d\n", result1 * result2);
+		return (0);
+	} else
+	{
+		printf("%s\n", "ERROR");
+		return (1);
+	}
+}
