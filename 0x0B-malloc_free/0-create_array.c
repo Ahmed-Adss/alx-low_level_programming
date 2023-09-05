1a2196320970bd3d76afe8314fe4ae0cdd990e2b
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-05 12:42:58 -0700

    malloc_free session

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,6 +14,8 @@ char *create_array(unsigned int size, char c)
 
 	char *ptr = (char *)malloc(sizeof(char) * size);
 
+	if (ptr == NULL) return(NULL);
+	if (size == 0) return(NULL);
 	for (i = 0; i < size; i++)
 
 		ptr[i] = c;
