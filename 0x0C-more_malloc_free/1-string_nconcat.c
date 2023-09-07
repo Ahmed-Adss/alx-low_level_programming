feec35b66f65b99014f433ebb69f760d0c6a3a83
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-07 15:40:45 -0700

    more_malloc_free session

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,6 +34,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = l2;
 	}
 	new = (char *)malloc(l1 + n + 1);
+	if (new == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < l1; i++)
 	{
 		new[i] = s1[i];
