dbe4a561f4b2559eb9d33cf6e29f0eb131e9bc94
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-07 15:46:16 -0700

    more_malloc_free session

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*_calloc - a function that allocates memory for an array, using malloc
+*@nmemb: unsigned int
+*@size: unsigned int
+*Return: always 0 if fail and pointer if success
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
