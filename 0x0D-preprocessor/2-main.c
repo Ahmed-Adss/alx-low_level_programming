927b63a4029244fb19ec99790f563b2042fa2473
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-11 14:02:32 -0700

    preprocessor session

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+*main- prints file name
+*Return: always 0 if compiled
+*/
 
-int main ()
+int main(void)
 {
-	printf("%s\n",__FILE__);
-	return(0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
