2f03dd87403d31a67afb29c933e86beaa5181101
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-11 14:56:10 -0700

    preprocessor session

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef FUN_H
-#define FUN_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-
-#ifndef ABS
-#define ABS(x) (x * ((x > 0) - (x < 0)))
-#endif
-
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
