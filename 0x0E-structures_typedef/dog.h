e7032a2363fbb707cf6e952ac5a9c505dfe1e5cf
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-11 16:40:59 -0700

    structures_typedef session

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,19 @@
-struct dog {
+#ifndef dog
+#define dog
+/**
+*struct dog- define a new type of var
+*@name: struct name
+*@age: dog age
+*@owner: dog owner name
+*Return: always 0
+*/
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+
+#endif
