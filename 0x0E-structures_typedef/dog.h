d38f665839de79e8b958e57cf330210267d0c5a2
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-11 23:53:23 -0700

    structures_typedef session

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,42 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-/**
-*struct dog- define a new type of var
-*@name: struct name
-*@age: dog age
-*@owner: dog owner name
-*Return: always 0
-*/
 
 struct dog
 {
 	char *name;
 	float age;
-	char *owner;
+	char * owner;
 };
 
-/**
- * dog_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 
 
 
-
-
-
-
-
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 #endif
