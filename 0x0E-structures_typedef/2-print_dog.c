2c284e56f3cdd5094867fb05be60fac42dcba6e4
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-12 02:00:52 -0700

    structures_typedef session

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,9 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", d->name ? d->name : "(nill)");
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	}else
+	{
+		return;
 	}
 
 
