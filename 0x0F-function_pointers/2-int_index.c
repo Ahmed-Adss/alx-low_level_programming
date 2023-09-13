4e6a293dec53edb2848386541de3451102cdb283
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-13 16:59:20 -0700

    function_pointers session

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,15 +18,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if(cmp(array[i]))
+			if (cmp(array[i]))
 			{
 				break;
 			}
-			
+
 		}
-		return(i);
+		return (i);
 	} else
 	{
-		return(-1);
+		return (-1);
 	}
 }
