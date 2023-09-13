1c2f5d8e6432af30d5e506678810f7bf4c6a13e7
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-13 16:49:45 -0700

    function_pointers session

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+
+
+/**
+*int_index- a function that prints a name
+*@array: input array
+*@size: size of array
+*@cmp: pointer to a function
+*Return: 0 always success
+*/
+
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if(cmp(array[i]))
+			{
+				break;
+			}
+			
+		}
+	}
+	return(i);
+}
