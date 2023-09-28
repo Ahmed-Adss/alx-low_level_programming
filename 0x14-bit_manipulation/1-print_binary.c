1884ba532a311641c00dc2d8ca4f4c05e861286e
Ahmed Ibrahim <eng.ahmedads@gmail.com>
2023-09-28 08:43:55 -0700

    bit_manipulation session

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * char string
+ *
+ * convert a binary number to an unsigned int
+ * 
+ * converted decimal number or 0 if there is an unconvertable char
+ */
+
+
+void print_binary(unsigned long int n)
+{
+
+
+
+
+
+
+
+
+
+
+}
